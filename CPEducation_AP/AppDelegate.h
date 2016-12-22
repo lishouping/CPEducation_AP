8c3dd3814af21cdea5780c92222197f223fe778a
lishouping <lishouping@lishoupingdemacbook-pro.local>
2016-12-22 08:49:00 +0800

    Initial Commit

---
+++ CPEducation_AP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CPEducation_AP
+//
+//  Created by lishouping on 16/12/22.
+//  Copyright © 2016年 李寿平. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CPEducation_AP/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CPEducation_AP
+//
+//  Created by lishouping on 16/12/22.
+//  Copyright © 2016年 李寿平. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
